7f47129dbc084ddc6e77169c5f0ddc8f75182298
126511 <126511@dgwcloud.nl>
2021-03-02 16:54:27 +0100

    removed some useless files

--- hello/__init__.py
+++ hello/__init__.py
@@ -4,10 +4,10 @@ import check50.c
 @check50.check()
 def exists():
     """hello.c exists"""
-    check50.exists("hello.c")
+    check50.exists("main.c")
 
 @check50.check(exists)
 def prints_hello():
     """prints "hello, world\\n" """
     # regex=True by default :)
-    check50.run("python3 hello.py").stdout("[Hh]ello, world!?\n").exit(0)
+    check50.run("python3 hello.py").stdout("[Hh]ello world\n").exit(0)
