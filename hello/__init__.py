5202952c314a2e8117046dd0703f2d1c7e2a4f9a
126511 <126511@dgwcloud.nl>
2021-03-02 17:01:43 +0100

    updated hello problem

--- hello/__init__.py
+++ hello/__init__.py
@@ -3,11 +3,16 @@ import check50.c
 
 @check50.check()
 def exists():
-    """hello.c exists"""
+    """main.c exists"""
     check50.exists("main.c")
+
+@check50.check(exists)
+def compiles():
+    """main.c compiles"""
+    check50.c.compile("main.c", lcs50=True)
 
 @check50.check(exists)
 def prints_hello():
     """prints "hello, world\\n" """
     # regex=True by default :)
-    check50.run("python3 hello.py").stdout("[Hh]ello world\n").exit(0)
+    check50.run("./main").stdout("[Hh]ello world\n").exit(0)
