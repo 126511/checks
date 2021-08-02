8f0040c91bf698d0c5d8b98671e4d8b56c0ea340
Vincent Ruijgrok <55142098+126511@users.noreply.github.com>
2021-08-02 23:14:47 +0200

    Update __init__.py

--- variabelen/__init__.py
+++ variabelen/__init__.py
@@ -1,1 +1,34 @@
+import check50
 
+
+@check50.check()
+def exists():
+    """variabelen.txt bestaat"""
+    check50.exists("log.sql", "answers.txt")
+    
+@check50.check(exists)
+def integer():
+    """variabele geheel getal goed gedeclareerd"""
+
+    text = open("variabelen.txt").read().lower()
+    if "int aantal_ijsjes = 6;" not in text:
+        raise check50.Failure(f"Variabele geheel getal niet goed gedeclareerd.")
+
+@check50.check(exists)
+def character():
+    """variabele karakter goed gedeclareerd"""
+
+    text = open("variabelen.txt").read().lower()
+    if "char schreeuw = '!';" not in text:
+        raise check50.Failure(f"Variabele karakter niet goed gedeclareerd.")
+
+@check50.check(exists)
+def string():
+    """variabele stuk tekst goed gedeclareerd"""
+
+    text = open("variabelen.txt").read().lower()
+    if "string test = \"Dit is een test\";" not in text:
+        raise check50.Failure(f"Variabele stuk tekst niet goed gedeclareerd.")
+
+        
+        
