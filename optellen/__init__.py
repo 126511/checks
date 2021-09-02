fa22aca05fa5d00c1e1b9a0dd34a83fb1e65e229
126511 <126511@dgwcloud.nl>
2021-09-02 12:00:24 +0200

    optellen toegevoegd

---
+++ optellen/__init__.py
@@ -0,0 +1,27 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """hello.c bestaat"""
+    check50.exists("main.c")
+
+@check50.check(exists)
+def compiles():
+    """hello.c compileert"""
+    check50.c.compile("main.c", lcs50=True)
+
+@check50.check(compiles)
+def hundred():
+    """4 + 5 = 9"""
+    check50.run("./main").stdin("4").stdin("5").stdout("9").exit()
+
+@check50.check(compiles)
+def eighty():
+    """1 + 2 = 3"""
+    check50.run("./main").stdin("1").stdin("2").stdout("3").exit()
+    
+@check50.check(compiles)
+def sixty():
+  """100 + 200 = 300"""
+  check50.run("./main").stdin("100").stdin("200").stdout("300").exit()
