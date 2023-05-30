184b7f033e4aab223701a00dc8a732d36a951af3
Vincent Ruijgrok <55142098+126511@users.noreply.github.com>
2023-05-30 12:12:43 +0200

    Update __init__.py

--- 1/__init__.py
+++ 1/__init__.py
@@ -1,1 +1,23 @@
+import check50
+
+
+@check50.check()
+def exists():
+    """main.py exists"""
+    check50.exists("main.py")
+
+@check50.check(exists)
+def testNegative():
+    """print een 0 voor een negatief getal"""
+    check50.run("python3 main.py").stdin("-5").stdout("0").exit()
+
+@check50.check(exists)
+def test10():
+    """input van 10 geeft 20"""
+    check50.run("python3 main.py").stdin("10").stdout("20").exit()
+
+@check50.check(exists)
+def test9():
+    """input van 9 geeft 20"""
+    check50.run("python3 main.py").stdin("9").stdout("20").exit()
 
